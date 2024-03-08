5d5f0919cdbdeb3a3e97fb4f5771273c09354a5d
Gonzalo GS <gonzalo17004@hotmail.com>
2024-03-08 15:45:09 -0500

    se agrego el el multiusuario

--- views/layout/page_menu.php
+++ views/layout/page_menu.php
@@ -1,12 +1,17 @@
-    <div class="mobile-menu-left-overlay"></div>
-    <nav class="side-menu">
-        <ul class="side-menu-list">
+<div class="mobile-menu-left-overlay"></div>
+<nav class="side-menu">
+    <ul class="side-menu-list">
+        <?php
+        if ($_SESSION["ID_roles"] == 1) {
+        ?>
+            <!-- Tablero -->
             <li class="blue-dirty">
                 <a href="<?php echo Conectar::ruta() . "views/tablero/" ?>">
                     <span class="glyphicon glyphicon-th"></span>
                     <span class="lbl">Tablero</span>
                 </a>
             </li>
+            <!-- Reportes -->
             <li class="blue-dirty with-sub">
                 <span>
                     <span class="glyphicon glyphicon-th"></span>
@@ -19,15 +24,15 @@
                 <ul>
                     <li><a href="<?php echo Conectar::ruta() . "views/Reportes/general.php" ?>"><span class="lbl">General</span></a></li>
                 </ul>
-
             </li>
+            <!-- Gestion de usuario -->
             <li class="blue-dirty">
                 <a href="<?php echo Conectar::ruta() . "views/persona/consulta.php" ?>">
                     <span class="glyphicon glyphicon-th"></span>
                     <span class="lbl">Gestion de Usuario</span>
                 </a>
             </li>
-
+            <!-- Inventario-->
             <li class="blue-dirty with-sub">
                 <span>
                     <span class="glyphicon glyphicon-th"></span>
@@ -39,6 +44,33 @@
                 </ul>
 
             </li>
-        </ul>
+        <?php
+        } elseif ($_SESSION["ID_roles"] == 2) {
+        ?>
+            <!-- Tablero -->
+            <li class="blue-dirty">
+                <a href="<?php echo Conectar::ruta() . "views/tablero/" ?>">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Tablero</span>
+                </a>
+            </li>
+            <!-- Reportes -->
+            <li class="blue-dirty with-sub">
+                <span>
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Reportes</span>
+                </span>
 
-    </nav>
+                <ul>
+                    <li><a href="<?php echo Conectar::ruta() . "views/Reportes/stock.php" ?>"><span class="lbl">Stock</span></a></li>
+                </ul>
+                <ul>
+                    <li><a href="<?php echo Conectar::ruta() . "views/Reportes/general.php" ?>"><span class="lbl">General</span></a></li>
+                </ul>
+            </li>
+        <?php
+        }
+        ?>
+    </ul>
+
+</nav>
